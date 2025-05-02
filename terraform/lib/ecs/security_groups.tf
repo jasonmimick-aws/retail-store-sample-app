c30c4466e8f1311d3199eca890ac0621fa743c52
ninche21 <93350979+ninche21@users.noreply.github.com>
2025-05-01 22:40:40 -0400

    Create security_groups.tf

---
+++ terraform/lib/ecs/security_groups.tf
@@ -0,0 +1,81 @@
+resource "aws_security_group" "checkout" {
+  name        = "${var.environment_name}-checkout-service"
+  description = "Security group for checkout service"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.tags
+}
+
+resource "aws_security_group" "catalog" {
+  name        = "${var.environment_name}-catalog-service"
+  description = "Security group for catalog service"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.tags
+}
+
+resource "aws_security_group" "cart" {
+  name        = "${var.environment_name}-cart-service"
+  description = "Security group for cart service"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.tags
+}
+
+resource "aws_security_group" "orders" {
+  name        = "${var.environment_name}-orders-service"
+  description = "Security group for orders service"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.tags
+}
+
+resource "aws_security_group" "ui" {
+  name        = "${var.environment_name}-ui-service"
+  description = "Security group for UI service"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.tags
+}
