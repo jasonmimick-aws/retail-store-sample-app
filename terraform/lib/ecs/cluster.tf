91f4556a932a478a5935f4c205dad838cbfc0a4d
ninche21 <93350979+ninche21@users.noreply.github.com>
2025-05-01 23:20:08 -0400

    Update cluster.tf

--- terraform/lib/ecs/cluster.tf
+++ terraform/lib/ecs/cluster.tf
@@ -11,10 +11,22 @@ resource "aws_ecs_cluster" "cluster" {
 
 resource "aws_cloudwatch_log_group" "ecs_tasks" {
   name = "${var.environment_name}-tasks"
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes = [
+      name,
+      retention_in_days
+    ]
+  }
 }
 
 resource "aws_service_discovery_private_dns_namespace" "this" {
   name        = "retailstore.le.local"
   description = "Service discovery namespace"
   vpc         = var.vpc_id
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
